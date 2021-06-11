648b9d668a0525e76c71d68c05f384c486702fe5
Samy CHOUAM <sachouam@e2r4p22.42.fr>
2021-06-11 18:58:34 +0200

    push_swap 11 06 2021 part 2

--- push_swap_srcs/push_swap.c
+++ push_swap_srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sachouam <sachouam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 17:01:55 by sachouam          #+#    #+#             */
-/*   Updated: 2021/06/11 11:33:54 by sachouam         ###   ########.fr       */
+/*   Updated: 2021/06/11 18:45:37 by sachouam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -197,6 +197,33 @@ void
 		ft_reverse_rotate_stack(a, 'a');
 }
 */
+
+
+static int
+	ft_find_third_greater(t_stack **root)
+{
+	int		nbr;
+	t_stack	*cpy;
+	t_stack	*curr;
+
+	cpy = (*root)->next;
+	while (cpy != *root)
+	{
+		curr = (*root)->next;
+		nbr = 0;
+		while (curr != *root)
+		{
+			if (curr->integer > cpy->integer)
+				nbr++;
+			curr = curr->next;
+		}
+		if (nbr == 2)
+			return (cpy->integer);
+		cpy = cpy->next;
+	}
+	return (0);
+}
+
 static void
 	ft_print_stack(t_stack **stack_a, t_stack **stack_b)
 {
@@ -207,24 +234,33 @@ static void
 	int		even;
 	int		half;
 	int		i;
+	int		third;
 
 	med = ft_find_mediane(stack_a);
 	even = ft_is_number_of_int_even(stack_a);
 	half = ft_stack_size(*stack_a) / 2;
-	printf("med = %d\n", med);
-	printf("even = %d\n", even);
-	printf("half = %d\n", half);
+	third = ft_find_third_greater(stack_a);
+	//printf("med = %d\n", med);
+	//printf("even = %d\n", even);
+	//printf("half = %d\n", half);
+	//printf("third greater bf push = %d\n", third);
 	//ft_how_you_rotate_two(med, half, stack_a, 'a');
-	printf("1\n");
+	//printf("1\n");
 	if (ft_stack_size(*stack_a) > 3)
+	{
 		ft_push_b_or_rotate(stack_a, stack_b, med);
-	printf("2\n");
+	//printf("2\n");
+		ft_push_b_or_rotate(stack_a, stack_b, third);
+	}
+	//printf("min = %d\nmax = %d\n", ft_find_min(stack_a), ft_find_max(stack_a));
+	//printf("third greater = %d\n", ft_find_third_greater(stack_a));
+	//printf("3\n");
 	ft_sort_three_integers(stack_a);
-	printf("3\n");
+	//printf("4\n");
 	if ((*stack_b)->next != *stack_b)
 		ft_rotate_b_or_push_a(stack_a, stack_b);
 	//ft_reverse_rotate_both(stack_a, stack_b);
-	//ft_reverse_rotate_stack(stack_a, 'a');²
+	//ft_reverse_rotate_stack(stack_a, 'a');
 	//ft_rotate_stack(stack_a, 'a');
 	//ft_swap_stack(stack_a, 'a');
 	//ft_push_stack_b(stack_a, stack_b, 'b');
