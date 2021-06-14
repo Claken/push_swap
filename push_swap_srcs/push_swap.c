6bdb862c5f4c4307f767e84d6d4cd4be14e8b759
Claken <samychouam@gmail.com>
2021-06-14 11:28:25 +0200

    push_swap 14 06 2021

--- push_swap_srcs/push_swap.c
+++ push_swap_srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sachouam <sachouam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 17:01:55 by sachouam          #+#    #+#             */
-/*   Updated: 2021/06/12 20:10:27 by sachouam         ###   ########.fr       */
+/*   Updated: 2021/06/14 11:26:14 by sachouam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,8 @@ int
 		return (0);
 	stack_a = ft_create_list();
 	stack_b = ft_create_list();
+	if (stack_a == NULL || stack_b == NULL)
+		return (0);
 	if (!ft_create_stack_a(&stack_a, ac, av))
 		return (0);
 	ft_sorting_stack_a(&stack_a, &stack_b);
